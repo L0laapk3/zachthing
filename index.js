639d85ff24481e05372995ee023151df183a0d0d
Cristina Marques <ccmarques98@gmail.com>
2024-01-31 23:52:12 +0100

    fucked around

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
 
 let mouseX = 0;
 let mouseY = 0;
@@ -22,12 +26,27 @@ let teams = [
 ];
 let pawns = [];
 for (let i = 0; i < teams.length; i++)
-	pawns.push(new Pawn(100 + 50 * i, 100, teams[i]));
+	pawns.push(new Pawn(100 + 75 * i, 100, teams[i]));
 
-let stars = [new Star(200, 200, teams[0])];
+let stars = [];
+for (let i = 0; i < teams.length; i++)
+	stars.push(new Star(100 + 75 * i, 200, teams[i]));
 
 
+
+const stepSize = 10;
+let lastUpdateTime = new Date().getTime();
+let lastDrawTime = new Date().getTime();
 function draw() {
+	const t = new Date().getTime(), dt = t - lastDrawTime;
+	lastDrawTime = t;
+
+	// run steps
+	while (lastUpdateTime < t) {
+		update(stepSize);
+		lastUpdateTime += stepSize;
+	}
+
 	// clear the canvas
 	ctx.fillStyle = "#111";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -50,13 +69,18 @@ function draw() {
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 	for (let pawn of pawns)
-		pawn.draw(ctx);
+		pawn.draw(ctx, dt);
 	for (let star of stars)
-		star.draw(ctx);
-}
+		star.draw(ctx, dt);
 
-function update() {
-	draw();
-	requestAnimationFrame(update);
+	requestAnimationFrame(draw);
 }
-update();
+draw();
+
+
+function update(dt) {
+	for (let pawn of pawns)
+		pawn.update(dt);
+	for (let star of stars)
+		star.update(dt);
+}
--- pawn.js
+++ pawn.js
@@ -14,6 +14,7 @@ class Team {
 
 	drawSetup(ctx) {
 		ctx.fillStyle = this.color;
+		ctx.strokeStyle = "#0000";
 		ctx.shadowColor = this.color;
 	}
 }
@@ -39,7 +40,10 @@ class Pawn extends TeamObject {
 		ctx.shadowBlur = 10;
 	}
 
-	draw(ctx) {
+	update(dt) {
+	}
+
+	draw(ctx, dt) {
 		this.drawSetup(ctx);
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, 3, 0, Math.PI * 2, false);
@@ -50,19 +54,68 @@ class Pawn extends TeamObject {
 class Star extends TeamObject {
 	constructor(x, y, team) {
 		super(x, y, team);
+		this.animationTime = 0;
+		this.angOffset = Math.PI / 2;//Math.random() * Math.PI * 2;
+
 		this.charge = 0;
+		this.chargeSpeed = Math.random() * .002;
 	}
 
 	drawSetup(ctx) {
 		super.drawSetup(ctx);
-		ctx.shadowBlur = 25;
+		ctx.shadowBlur = 30;
 	}
 
-	draw(ctx) {
-		this.drawSetup(ctx);
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, 20, 0, Math.PI * 2, false);
-		ctx.fill();
-		ctx.stroke();
+	update(dt) {
+		this.charge += dt * this.chargeSpeed;
 	}
+
+	draw(ctx, dt) {
+    this.drawSetup(ctx);
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 20, 0, Math.PI * 2, false);
+		ctx.fill();
+    ctx.stroke();
+
+		this.animationTime += dt * this.chargeSpeed;
+		ctx.shadowBlur = 0;	
+		ctx.beginPath();
+		let ang = Math.PI * (this.animationTime % 2);
+		ctx.arc(this.x + Math.cos(this.angOffset + ang) * 20, this.y + Math.sin(this.angOffset + ang) * 20, 3, 0, Math.PI * 2, false);
+		ctx.arc(this.x + Math.cos(this.angOffset - ang) * 20, this.y + Math.sin(this.angOffset - ang) * 20, 3, 0, Math.PI * 2, false);
+		ctx.fill();
+	
+		ctx.stroke();
+		ctx.strokeStyle = "#0000";
+		ctx.lineWidth = 1;
+
+		// text box
+		const boxWidth = 22, boxHeight = 14, cornerRadius = 5;
+		ctx.fillStyle = "#111";
+		ctx.beginPath();
+		ctx.moveTo(this.x - boxWidth / 2 + cornerRadius, this.y - boxHeight / 2);
+		ctx.arcTo(this.x + boxWidth / 2, this.y - boxHeight / 2, this.x + boxWidth / 2, this.y + boxHeight / 2, cornerRadius);
+		ctx.arcTo(this.x + boxWidth / 2, this.y + boxHeight / 2, this.x - boxWidth / 2, this.y + boxHeight / 2, cornerRadius);
+		ctx.arcTo(this.x - boxWidth / 2, this.y + boxHeight / 2, this.x - boxWidth / 2, this.y - boxHeight / 2, cornerRadius);
+		ctx.arcTo(this.x - boxWidth / 2, this.y - boxHeight / 2, this.x + boxWidth / 2, this.y - boxHeight / 2, cornerRadius);
+		ctx.closePath();
+		ctx.fill();
+
+		// text
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.fillStyle = this.team.color;
+		ctx.fillText(Math.floor(this.charge), this.x, this.y);
+	}
+}
+
+
+class Base extends TeamObject {
+	constructor(x, y, team) {
+		super(x, y, team);
+		this.charge = 0;
+	}
+
+
 }
